c75fc05dbf2c654608a7cd31922251f2221209c2
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-06-29 20:23:40 +0200

    change wording of test cases

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-describe('class CssSelectorExtract', () => {
+describe('CssSelectorExtract', () => {
   const cssSelectorExtract = require('../');
   const expect = require('chai').expect;
   const fs = require('fs');
@@ -17,7 +17,7 @@ describe('class CssSelectorExtract', () => {
     /**
      * .test1
      */
-    it('correct way to extract default selector: returns `.test1` selector', () => {
+    it('correct way to extract default selector: should return `.test1` selector', () => {
       let selectors = ['.test1'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal(`.test1 {
@@ -29,7 +29,7 @@ describe('class CssSelectorExtract', () => {
     /**
      * .test2
      */
-    it('correct way to extract nested selector (CSS): returns `.nested .test2` selector', () => {
+    it('correct way to extract nested selector (CSS): should return `.nested .test2` selector', () => {
       let selectors = ['.nested .test2'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal(`.nested .test2 {
@@ -38,7 +38,7 @@ describe('class CssSelectorExtract', () => {
       });
     });
 
-    it('wrong way to extract nested selector (CSS): returns empty string', () => {
+    it('wrong way to extract nested selector (CSS): should return empty string', () => {
       let selectors = ['.test2'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal('');
@@ -48,7 +48,7 @@ describe('class CssSelectorExtract', () => {
     /**
      * .test3
      */
-    it('correct way to extract nested selector (SCSS): returns `.nested .test3` selector', () => {
+    it('correct way to extract nested selector (SCSS): should return `.nested .test3` selector', () => {
       let selectors = ['.nested', '.test3'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal(`.nested {
@@ -59,7 +59,7 @@ describe('class CssSelectorExtract', () => {
       });
     });
 
-    it('wrong way to extract nested selector (SCSS): returns empty string', () => {
+    it('wrong way to extract nested selector (SCSS): should return empty string', () => {
       let selectors = ['.test3'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal('');
@@ -69,7 +69,7 @@ describe('class CssSelectorExtract', () => {
     /**
      * .test4
      */
-    it('correct way to extract @media nested selector (CSS): returns `@media .test4` selector', () => {
+    it('correct way to extract @media nested selector (CSS): should return `@media .test4` selector', () => {
       let selectors = ['.test4'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal(`@media (min-width: 30em) {
@@ -83,7 +83,7 @@ describe('class CssSelectorExtract', () => {
     /**
      * .test5
      */
-    it('correct way to extract @media nested selector (SCSS): returns `.test5 @media` selector', () => {
+    it('correct way to extract @media nested selector (SCSS): should return `.test5 @media` selector', () => {
       let selectors = ['.test5'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal(`.test5 {
@@ -106,7 +106,7 @@ describe('class CssSelectorExtract', () => {
       expect(typeof cssSelectorExtract.prototype._postcssSelectorExtract).to.equal('function');
     });
 
-    it('returns postcss plugin named "postcss-extract-selectors"', () => {
+    it('should return a postcss plugin named "postcss-extract-selectors"', () => {
       expect(cssSelectorExtract.prototype._postcssSelectorExtract().postcss).to.not.be.undefined;
       expect(cssSelectorExtract.prototype._postcssSelectorExtract().postcss.postcssPlugin).to.equal('postcss-extract-selectors');
     });
