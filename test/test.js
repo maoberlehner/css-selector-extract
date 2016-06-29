250fea43f39838038771137781a0713e22e02713
Markus Oberlehner <markus.oberlehner@gmail.com>
2016-06-29 20:06:58 +0200

    use es6 syntax for test.js code

--- test/test.js
+++ test/test.js
@@ -1,32 +1,32 @@
-describe('class CssSelectorExtract', function() {
-  var cssSelectorExtract = require('../');
-  var expect = require('chai').expect;
-  var fs = require('fs');
+describe('class CssSelectorExtract', () => {
+  const cssSelectorExtract = require('../');
+  const expect = require('chai').expect;
+  const fs = require('fs');
 
-  var css = fs.readFileSync('test/test.scss').toString();
+  const css = fs.readFileSync('test/test.scss').toString();
 
-  it('should be a function', function(){
+  it('should be a function', () => {
     expect(typeof cssSelectorExtract).to.equal('function');
   });
 
-  it('should have a process method', function(){
+  it('should have a process method', () => {
     expect(typeof cssSelectorExtract.process).to.equal('function');
   });
 
-  it('should have a processSync method', function(){
+  it('should have a processSync method', () => {
     expect(typeof cssSelectorExtract.processSync).to.equal('function');
   });
 
-  it('should have a prototype._postcssSelectorExtract method', function(){
+  it('should have a prototype._postcssSelectorExtract method', () => {
     expect(typeof cssSelectorExtract.prototype._postcssSelectorExtract).to.equal('function');
   });
 
-  describe('#process()', function() {
+  describe('#process()', () => {
     /**
      * .test1
      */
-    it('correct way to extract default selector: returns `.test1` selector', function() {
-      var selectors = ['.test1'];
+    it('correct way to extract default selector: returns `.test1` selector', () => {
+      let selectors = ['.test1'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal(`.test1 {
   content: 'Test 1';
@@ -37,8 +37,8 @@ describe('class CssSelectorExtract', function() {
     /**
      * .test2
      */
-    it('correct way to extract nested selector (CSS): returns `.nested .test2` selector', function() {
-      var selectors = ['.nested .test2'];
+    it('correct way to extract nested selector (CSS): returns `.nested .test2` selector', () => {
+      let selectors = ['.nested .test2'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal(`.nested .test2 {
   content: 'Test 2';
@@ -46,8 +46,8 @@ describe('class CssSelectorExtract', function() {
       });
     });
 
-    it('wrong way to extract nested selector (CSS): returns empty string', function() {
-      var selectors = ['.test2'];
+    it('wrong way to extract nested selector (CSS): returns empty string', () => {
+      let selectors = ['.test2'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal('');
       });
@@ -56,8 +56,8 @@ describe('class CssSelectorExtract', function() {
     /**
      * .test3
      */
-    it('correct way to extract nested selector (SCSS): returns `.nested .test3` selector', function() {
-      var selectors = ['.nested', '.test3'];
+    it('correct way to extract nested selector (SCSS): returns `.nested .test3` selector', () => {
+      let selectors = ['.nested', '.test3'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal(`.nested {
   .test3 {
@@ -67,8 +67,8 @@ describe('class CssSelectorExtract', function() {
       });
     });
 
-    it('wrong way to extract nested selector (SCSS): returns empty string', function() {
-      var selectors = ['.test3'];
+    it('wrong way to extract nested selector (SCSS): returns empty string', () => {
+      let selectors = ['.test3'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal('');
       });
@@ -77,8 +77,8 @@ describe('class CssSelectorExtract', function() {
     /**
      * .test4
      */
-    it('correct way to extract @media nested selector (CSS): returns `@media .test4` selector', function() {
-      var selectors = ['.test4'];
+    it('correct way to extract @media nested selector (CSS): returns `@media .test4` selector', () => {
+      let selectors = ['.test4'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal(`@media (min-width: 30em) {
   .test4 {
@@ -91,8 +91,8 @@ describe('class CssSelectorExtract', function() {
     /**
      * .test5
      */
-    it('correct way to extract @media nested selector (SCSS): returns `.test5 @media` selector', function() {
-      var selectors = ['.test5'];
+    it('correct way to extract @media nested selector (SCSS): returns `.test5 @media` selector', () => {
+      let selectors = ['.test5'];
       return cssSelectorExtract.process(css, selectors).then((extractCss) => {
         expect(extractCss.trim()).to.equal(`.test5 {
   @media (min-width: 30em) {
@@ -103,8 +103,8 @@ describe('class CssSelectorExtract', function() {
     });
   });
 
-  describe('#_postcssSelectorExtract()', function() {
-    it('returns postcss plugin named "postcss-extract-selectors"', function(){
+  describe('#_postcssSelectorExtract()', () => {
+    it('returns postcss plugin named "postcss-extract-selectors"', () => {
       expect(cssSelectorExtract.prototype._postcssSelectorExtract().postcss).to.not.be.undefined;
       expect(cssSelectorExtract.prototype._postcssSelectorExtract().postcss.postcssPlugin).to.equal('postcss-extract-selectors');
     });
